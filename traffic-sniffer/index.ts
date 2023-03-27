a3239a77c81d607904b772149fd649dbcf70e957
IdoDavidAK <ido@arborknot.io>
2023-03-27 21:47:23 +0300

    feat: command line arguments

--- traffic-sniffer/index.ts
+++ traffic-sniffer/index.ts
@@ -1,6 +1,11 @@
 import { Sniffer } from "./lib/request-metadata/sniffer/sniffer";
 
 require("dotenv/config");
+var args = process.argv.slice(2);
 
-const sniffer = new Sniffer({ port: 5012, proxyUrl: "http://localhost:3001" });
+const sniffer = new Sniffer({
+  port: +(args[0] ?? 5012),
+  proxyUrl: args[1] ?? "http://localhost:3000",
+});
+
 sniffer.start();
