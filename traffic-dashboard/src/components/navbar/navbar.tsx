a83c6af724e6e1d665567f46dde636e869657b69
Noam <80849999+noams24@users.noreply.github.com>
2023-06-19 16:18:41 +0300

    Added logo (#41)

--- traffic-dashboard/src/components/navbar/navbar.tsx
+++ traffic-dashboard/src/components/navbar/navbar.tsx
@@ -1,5 +1,8 @@
 import MenuIcon from "@mui/icons-material/Menu";
 import { AppBar, IconButton, Toolbar } from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import styles from "./navbar.module.scss";
+import React from "react";
 
 interface INavbarProps {
   onBurgerClicked: () => void;
@@ -8,17 +11,26 @@ interface INavbarProps {
 export const Navbar: React.FC<INavbarProps> = ({ onBurgerClicked }) => {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-          onClick={() => onBurgerClicked?.()}
-        >
-          <MenuIcon></MenuIcon>
-        </IconButton>
+      <Toolbar className={styles.navbar}>
+        <div className={styles.navbar_left}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 1 }}
+            onClick={() => onBurgerClicked?.()}
+          >
+            <MenuIcon></MenuIcon>
+          </IconButton>
+          <div className={styles.logo}>
+            <a href="/home"><img src="shark-logo.png" alt="Logo"/></a>
+          </div>
+          <div className={styles.logo_text}>
+            SHARKIO
+          </div>
+        </div>
+        <AccountCircleIcon fontSize="large"/>
       </Toolbar>
     </AppBar>
   );
