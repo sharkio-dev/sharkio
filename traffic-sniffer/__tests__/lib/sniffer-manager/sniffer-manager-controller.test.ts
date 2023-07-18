106c808e43ec4c96056ff5afd38d043107f31540
Nathaniel Kotzrenko <n.kotzrenko@gmail.com>
2023-07-18 16:13:33 +0300

    Fixed a test on the sniffer-manager module and added request-validator test file + removed comment

--- traffic-sniffer/__tests__/lib/sniffer-manager/sniffer-manager-controller.test.ts
+++ traffic-sniffer/__tests__/lib/sniffer-manager/sniffer-manager-controller.test.ts
@@ -61,7 +61,6 @@ describe("sniffer-manager-controlelr", () => {
       .set("Content-Type", "application/json")
       .send(snifferConfig);
 
-    console.log(response.body);
     expect(response.status).toEqual(201);
     expect(snifferManager.getAllSniffers().length).toEqual(1);
   });
