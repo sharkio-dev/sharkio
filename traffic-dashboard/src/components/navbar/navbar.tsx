afbef504a29f97d950b3dbf1b8061b5a63e47e43
OshriAsulin <114335827+OshriAsulin@users.noreply.github.com>
2023-07-28 09:54:26 +0200

    Added GitHub icon to navbar (#156)

--- traffic-dashboard/src/components/navbar/navbar.tsx
+++ traffic-dashboard/src/components/navbar/navbar.tsx
@@ -2,13 +2,22 @@ import { AppBar } from "@mui/material";
 import React from "react";
 import styles from "./navbar.module.scss";
 import LoginComponent from "../login-component/login-component";
-
+import GitHubIcon from "@mui/icons-material/GitHub";
 export const Navbar: React.FC = () => {
   return (
     <AppBar position="relative">
       <div className={styles.navbar}>
         <div className={styles.navbar_left} />
-        <LoginComponent />
+        <div className={styles.navbar_right}>
+          <a
+            className={styles.github_link}
+            href="https://github.com/idodav/sharkio"
+            target="_blank"
+          >
+            <GitHubIcon fontSize="large" />
+          </a>
+          <LoginComponent />
+        </div>
       </div>
     </AppBar>
   );
