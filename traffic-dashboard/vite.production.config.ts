a60bbab6d09cb2a05ab0de9ea251064d4b38ce50
Ido David <21335259+idodav@users.noreply.github.com>
2023-08-12 18:45:55 +0300

    feat: server config (#178)

---
+++ traffic-dashboard/vite.production.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 443,
+    proxy: {
+      '/sharkio': 'http://localhost:5012',
+    },
+    https: {
+      key: fs.readFileSync('/home/ido/certs/private.key'),
+      cert: fs.readFileSync('/home/ido/certs/certificate.crt'),
+    },
+  },
+});
