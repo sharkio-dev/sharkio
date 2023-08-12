086ae29a43e9f0da32c86c2c4c48dcc3adfe100b
Ido David <21335259+idodav@users.noreply.github.com>
2023-08-12 18:37:33 +0300

    feat: login with supabase (#177)
    * feat: login with supabase
    
    * feat: .env + login css

--- traffic-dashboard/src/App.tsx
+++ traffic-dashboard/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PageTemplate } from './components/page-template/page-template';
 import { routes } from './constants/routes';
 import { RequestMetadataProvider } from './context/requests-context';
+import AuthUI from './pages/auth/Auth';
 import { Config } from './pages/config/config';
 import { GenOpenAPI } from './pages/gen-openapi/gen-openapi';
 import { Home } from './pages/home/Home';
@@ -19,25 +20,28 @@ function App(): React.JSX.Element {
       mode: 'dark',
     },
   });
+
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <RequestMetadataProvider>
-          <PageTemplate>
-            <Routes>
-              <Route path={routes.HOME} element={<Home />} />
-              <Route path="/new-request" element={<NewRequest />}></Route>
-              <Route path={routes.REQUEST} element={<RequestPage />}></Route>
-              <Route path={routes.CONFIG} element={<Config />}></Route>
-              <Route path={routes.REQUESTS} element={<Requests />}></Route>
-              <Route path={routes.MOCKS} element={<MocksPage />}></Route>
-              <Route path={routes.SERVICE} element={<Service />}></Route>
-              <Route path={routes.MOCKS} element={<Mocks />}></Route>
-              <Route path={routes.OPENAPI} element={<GenOpenAPI />}></Route>
-              <Route path={'*'} element={<Home />}></Route>
-            </Routes>
-          </PageTemplate>
-        </RequestMetadataProvider>
+        <AuthUI>
+          <RequestMetadataProvider>
+            <PageTemplate>
+              <Routes>
+                <Route path={routes.HOME} element={<Home />} />
+                <Route path="/new-request" element={<NewRequest />}></Route>
+                <Route path={routes.REQUEST} element={<RequestPage />}></Route>
+                <Route path={routes.CONFIG} element={<Config />}></Route>
+                <Route path={routes.REQUESTS} element={<Requests />}></Route>
+                <Route path={routes.MOCKS} element={<MocksPage />}></Route>
+                <Route path={routes.SERVICE} element={<Service />}></Route>
+                <Route path={routes.MOCKS} element={<Mocks />}></Route>
+                <Route path={routes.OPENAPI} element={<GenOpenAPI />}></Route>
+                <Route path={'*'} element={<AuthUI />}></Route>
+              </Routes>
+            </PageTemplate>
+          </RequestMetadataProvider>
+        </AuthUI>
       </ThemeProvider>
     </BrowserRouter>
   );
--- traffic-dashboard/src/components/login-component/login-component.tsx
+++ traffic-dashboard/src/components/login-component/login-component.tsx
@@ -1,84 +1,70 @@
-import React, { useState } from 'react';
-import {
-  Avatar,
-  Button,
-  Menu,
-  MenuItem,
-  Tooltip,
-  Typography,
-} from '@mui/material';
+import { Avatar, Menu, MenuItem, Tooltip, Typography } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
+import React, { useEffect, useState } from 'react';
 import styles from './login-component.module.scss';
+import { supabaseClient } from '../../utils/supabase-auth';
 
 const LoginComponent: React.FC = () => {
-  const [login, setLogin] = useState(true);
   const [anchorElUser, setAnchorElUser] = useState(false);
 
-  const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+  const settings = ['Logout'];
 
   const handleOpenUserMenu = () => {
     setAnchorElUser(!anchorElUser);
   };
 
-  const handleCloseUserMenu = (setting: string) => {
+  const handleCloseUserMenu = async (setting: string) => {
     setAnchorElUser(!anchorElUser);
     if (setting === 'Logout') {
-      setLogin(true);
+      const { error } = await supabaseClient.auth.signOut();
     }
   };
 
+  useEffect(() => {
+    (async () => {
+      const {
+        data: { user },
+      } = await supabaseClient.auth.getUser();
+    })();
+  }, []);
+
   return (
     <div className={styles.login_component_container}>
-      {login ? (
-        <Button variant="outlined" onClick={() => setLogin(!login)}>
-          Login
-        </Button>
-      ) : (
-        <div>
-          <div onClick={handleOpenUserMenu} className={styles.my_box}>
-            <Tooltip title="Open settings">
-              <IconButton sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" sx={{ width: 34, height: 34 }} />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '55px' }}
-              id="menu-appbar"
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem
-                  key={setting}
-                  onClick={() => handleCloseUserMenu(setting)}
-                >
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </div>
+      <div>
+        <div onClick={handleOpenUserMenu} className={styles.my_box}>
+          <Tooltip title="Open settings">
+            <IconButton sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" sx={{ width: 34, height: 34 }} />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '55px' }}
+            id="menu-appbar"
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem
+                key={setting}
+                onClick={() => handleCloseUserMenu(setting)}
+              >
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
         </div>
-      )}
+      </div>
     </div>
   );
 };
 
 export default LoginComponent;
-
-// sx={{
-//     flexGrow: 0,
-//     border: 1,
-//     borderColor: "primary.main",
-//     cursor: "pointer",
-//     padding: "5px",
-//     borderRadius: "10px",
-//   }}
