914707da89949538e82239bcdce4c777c0c3b7bd
IdoDavidAK <ido@arborknot.io>
2023-08-12 22:35:18 +0300

    fix: remove unused

--- traffic-dashboard/src/pages/auth/Auth.tsx
+++ traffic-dashboard/src/pages/auth/Auth.tsx
@@ -1,11 +1,10 @@
+import { Typography } from '@mui/material';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { Session } from '@supabase/supabase-js';
 import React, { PropsWithChildren, useEffect, useState } from 'react';
 import { supabaseClient } from '../../utils/supabase-auth';
 import styles from './auth.module.scss';
-import { SharkSpinner } from '../../components/shark-spinner/shark-spinner';
-import { Typography } from '@mui/material';
 
 export const AuthUI: React.FC<PropsWithChildren> = ({ children }) => {
   const [session, setSession] = useState<Session | null>();
