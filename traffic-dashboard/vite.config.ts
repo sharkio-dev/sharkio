827cd049e6ba1abbeaf143c2b3334fbcdf763f5d
IdoDavidAK <ido@arborknot.io>
2023-08-21 09:50:05 +0300

    fix: github tests fix + localhost https

--- traffic-dashboard/vite.config.ts
+++ traffic-dashboard/vite.config.ts
@@ -1,14 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
-
-// https://vitejs.dev/config/
 export default () => {
   return defineConfig({
-    plugins: [react()],
+    plugins: [react(), basicSsl()],
     server: {
+      port: 443,
       proxy: {
-        // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
         '/sharkio': 'http://localhost:5012',
       },
     },
