9ba598f314325d4fb3f51b4856383cf8e3d7e4e1
Oshri Asulin <114335827+OshriAsulin@users.noreply.github.com>
2023-08-31 10:28:48 +0300

    fix- bug of preference reset on refresh #194 (#195)

--- traffic-dashboard/src/stores/themeStore.ts
+++ traffic-dashboard/src/stores/themeStore.ts
@@ -1,15 +1,24 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface themeStore {
   mode: "light" | "dark";
   toggleColorMode: () => void;
 }
 
-export const useThemeStore = create<themeStore>((set) => ({
-  mode: "light",
-  toggleColorMode: () => {
-    set((state) => ({
-      mode: state.mode === "light" ? "dark" : "light",
-    }));
-  },
-}));
+export const useThemeStore = create<themeStore>()(
+  persist(
+    (set, get) => ({
+      mode: "dark",
+      toggleColorMode: () => {
+        set((state) => ({
+          mode: state.mode === "light" ? "dark" : "light",
+        }));
+      },
+    }),
+    {
+      name: "theme-mode",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
