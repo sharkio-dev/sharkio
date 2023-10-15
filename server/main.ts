a6944e5d8bb01a14718f3ee8aed3cafaa7777f4c
IdoDavidAK <ido@arborknot.io>
2023-10-16 00:06:50 +0300

    chore: remove file config

--- server/main.ts
+++ server/main.ts
@@ -4,7 +4,6 @@ import { CollectionFilePersistency } from "./lib/collection-manager/collection-f
 import { CollectionManager } from "./lib/collection-manager/collection-manager";
 import { CollectionManagerController } from "./lib/collection-manager/collection-manager-controller";
 import { DbConfig } from "./lib/model/setup-config/db-config";
-import { FileConfig } from "./lib/model/setup-config/file-config";
 import { MockManagerController } from "./lib/sniffer-manager/mock-manager-controller";
 import { SnifferManager } from "./lib/sniffer-manager/sniffer-manager";
 import { SnifferManagerController } from "./lib/sniffer-manager/sniffer-manager-controller";
@@ -15,10 +14,6 @@ export const setupFilePath =
   process.env.SETUP_FILE_PATH ?? "./sniffers-setup.json";
 
 async function main() {
-  const fileConfig = new FileConfig(setupFilePath);
-  const config = fileConfig.getAllUsersConfig();
-  console.debug(config);
-
   const collectionFilePersistency = new CollectionFilePersistency(
     "./collections.json",
   );
