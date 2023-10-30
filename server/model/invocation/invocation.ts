8f101703e71e474d31be64edc76d0736916bbe1e
IdoDavidAK <ido@arborknot.io>
2023-10-30 17:32:02 +0200

    feat: invocation

---
+++ server/model/invocation/invocation.ts
@@ -0,0 +1,54 @@
+import {
+  Column,
+  DataSource,
+  Entity,
+  PrimaryGeneratedColumn,
+  Repository,
+} from "typeorm";
+import { useLog } from "../../lib/log";
+
+const log = useLog({
+  dirname: __dirname,
+  filename: __filename,
+});
+
+@Entity({ name: "invocation" })
+export class Invocation {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column({ name: "created_at" })
+  createdAt: Date;
+
+  @Column({ name: "updated_at" })
+  updatedAt: Date;
+
+  @Column({ name: "user_id" })
+  userId: string;
+
+  @Column({ name: "sniffer_id" })
+  snifferId: string;
+
+  @Column({ name: "request_id" })
+  requestId: string;
+
+  @Column()
+  url: string;
+
+  @Column()
+  method: string;
+
+  @Column({ type: "varchar" })
+  body: Record<string, any>;
+
+  @Column({ type: "varchar" })
+  headers: Record<string, any>;
+}
+
+export class InvocationRepository {
+  repository: Repository<Invocation>;
+
+  constructor(private readonly appDataSource: DataSource) {
+    this.repository = appDataSource.manager.getRepository(Invocation);
+  }
+}
