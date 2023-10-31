cc024149a1e58122b61601e6e641108eeb5c7cfc
yair <yair.haras@gmail.com>
2023-10-31 22:12:00 +0200

    Refactor

--- cli/commands/startSniffer.js
+++ cli/commands/startSniffer.js
@@ -1,12 +1,34 @@
 import ngrok from "ngrok";
 import chalk from "chalk";
+import express from "express";
+import cors from "cors";
+import pkg from "body-parser";
+const { json } = pkg;
+import cookieParser from "cookie-parser";
+import { urlencoded } from "express";
 import inquirer from "inquirer";
 import { getSniffers, patchSniffer } from "./api.js";
 
 const getLocalUrl = (port) => `http://localhost:${port}`;
 const getSnifferUrl = (name) => `https://${name}.localhost.sharkio.dev`;
 
-const startSniffer = async (params) => {
+class ProxyServer {
+  app;
+  constructor() {
+    this.app = express();
+    this.app.use(cors({ origin: "*" }));
+    this.app.use(json());
+    this.app.use(cookieParser());
+    this.app.use(urlencoded({ extended: true }));
+    // Add proxy middleware
+  }
+
+  async start(port) {
+    return this.app.listen(port);
+  }
+}
+
+const startSniffer = async () => {
   try {
     const sniffers = await getSniffers();
     const questions = [
@@ -20,6 +42,12 @@ const startSniffer = async (params) => {
           )}`,
           value: sniffer,
         })),
+        validate: (answer) => {
+          if (answer.length < 1) {
+            return "You must choose at least one option.";
+          }
+          return true;
+        },
       },
     ];
 
@@ -33,7 +61,8 @@ const startSniffer = async (params) => {
       );
       process.exit(1);
     }
-
+    const proxyServer = new ProxyServer();
+    const server = await proxyServer.start(50000);
     const url = await ngrok.connect(50000);
 
     selectedSniffers.forEach(async (sniffer) => {
@@ -46,9 +75,8 @@ const startSniffer = async (params) => {
     });
 
     selectedSniffers.forEach((sniffer) => {
-      // chalk.green(`\n🦈 Sniffer ${chalk.bold(sniffer.name)} is running!`);
-      const snifferUrl = `https://${sniffer.name}.localhost.sharkio.dev`;
-      const localUrl = `http://localhost:${sniffer.port}`;
+      const snifferUrl = getSnifferUrl(sniffer.name);
+      const localUrl = getLocalUrl(sniffer.port);
       console.log(
         chalk.green(
           `🌊 Forwarding ${chalk.bold(snifferUrl)} to ${chalk.bold(
