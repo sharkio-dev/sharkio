47390fb61c57de2e6f16637034d4bdacfa709283
yair <yair.haras@gmail.com>
2023-11-01 21:55:01 +0200

    Added getting started docs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,6 +15,8 @@ import { supabaseClient } from "./utils/supabase-auth";
 import { setAuthCookie } from "./api/api";
 import SniffersPage from "./pages/sniffers/SniffersPage";
 import { a11yDark, CopyBlock } from "react-code-blocks";
+import { TfiSettings } from "react-icons/tfi";
+import { AiOutlinePlayCircle } from "react-icons/ai";
 
 function App(): React.JSX.Element {
   const { mode } = useThemeStore();
@@ -105,7 +107,7 @@ function App(): React.JSX.Element {
               path={routes.DOCS}
               element={
                 <PageTemplate>
-                  <SharkioDocs />
+                  <SharkioDocsGettingStartedPage />
                 </PageTemplate>
               }
             />
@@ -159,135 +161,131 @@ const LandingPage = () => {
   );
 };
 
-const SharkioDocs = () => {
+const SharkioDocsSetupPage = () => {
   return (
-    <div className="flex flex-row w-full h-full">
-      <div className="flex h-full flex-col w-1/5 border-r border-border-color hidden sm:inline-flex ">
-        <div className="flex text-2xl font-bold font-mono mb-4 text-center bg-secondary p-2 px-4 rounded-lg">
-          Setup
+    <SharkioDocsPageTemplate
+      title={"Setup Guide"}
+      subTitle={
+        "Learn how to set up Sharkio and start using it to develop your APIs."
+      }
+    >
+      <SharkioDocsSection title="Sign Up" sectionNumber={1}>
+        Head to the{" "}
+        <a href="https://sharkio.dev" className="hover:text-blue-200">
+          Sharkio website.
+        </a>
+        <img
+          src="login.png"
+          alt=""
+          className="w-full rounded-lg h-96 border-2 border-border-color mt-4"
+        />
+      </SharkioDocsSection>
+
+      <SharkioDocsSection title="Generate an API Token" sectionNumber={2}>
+        <p>
+          Access the{" "}
+          <a
+            href="https://sharkio.dev/api-keys"
+            className="hover:text-blue-200"
+          >
+            API Keys page
+          </a>{" "}
+          by clicking on your profile picture and generate a token.
+        </p>
+        <img
+          src="apiKeys.png"
+          alt=""
+          className="w-full rounded-lg h-96 border-2 border-border-color mt-4"
+        />
+      </SharkioDocsSection>
+
+      <SharkioDocsSection title="Install Sharkio-cli" sectionNumber={3}>
+        Install the Sharkio-cli.
+        <div className="flex flex-col w-full mt-2">
+          <CopyBlock
+            language="bash"
+            text={`npm i -g sharkio-cli`}
+            showLineNumbers={false}
+            theme={a11yDark}
+            wrapLines={true}
+            codeBlock
+          />
         </div>
-        <div className="flex text-2xl font-bold font-mono mb-4 text-center bg-secondary p-2 px-4 rounded-lg">
-          Usage
+      </SharkioDocsSection>
+
+      <SharkioDocsSection title="Sharkio Login" sectionNumber={4}>
+        Login to Sharkio-cli, the session will be saved in your home folder.
+        <div className="flex flex-col w-full my-2">
+          <CopyBlock
+            language="bash"
+            text={`sharkio login`}
+            showLineNumbers={false}
+            theme={a11yDark}
+            wrapLines={true}
+            codeBlock
+          />
         </div>
-      </div>
-      <div className="p-4 flex flex-col flex-1">
-        <div className="flex text-4xl font-mono ">Setup Guide</div>
-        <div className="flex w-full border-b border-border-color my-6"></div>
-        <div className="text-lg font-mono mb-4 text-[#717171]">
-          Learn how to set up Sharkio and start using it to develop your APIs.
+        {"You'll"} be prompted to enter your email and the precious token{" "}
+        {"you've "}secured.
+      </SharkioDocsSection>
+    </SharkioDocsPageTemplate>
+  );
+};
+
+const SharkioDocsGettingStartedPage = () => {
+  return (
+    <SharkioDocsPageTemplate
+      title={"Getting Started"}
+      subTitle={"Get started with Sharkio in 4 easy steps."}
+    >
+      <SharkioDocsSection title={"Start Your API"} sectionNumber={1}>
+        Start your API service on your local machine.
+      </SharkioDocsSection>
+
+      <SharkioDocsSection title="Create a Sniffer" sectionNumber={2}>
+        Create a sniffer (=Proxy) to monitor your API traffic by running the
+        following command:
+        <div className="flex flex-col w-full my-2">
+          <CopyBlock
+            language="bash"
+            text={`sharkio create sniffer -p <port> -n <name>`}
+            showLineNumbers={false}
+            theme={a11yDark}
+            wrapLines={true}
+            codeBlock
+          />
         </div>
-
-        <div className="flex flex-col flex-1 p-4">
-          <SharkioDocsSection title="Sign Up" sectionNumber={1}>
-            Head to the{" "}
-            <a href="https://sharkio.dev" className="hover:text-blue-200">
-              Sharkio website.
-            </a>
-            <img
-              src="login.png"
-              alt=""
-              className="w-full rounded-lg h-96 border-2 border-border-color mt-4"
-            />
-          </SharkioDocsSection>
-
-          <SharkioDocsSection title="Generate an API Token" sectionNumber={2}>
-            <p>
-              Access the{" "}
-              <a
-                href="https://sharkio.dev/api-keys"
-                className="hover:text-blue-200"
-              >
-                API Keys page
-              </a>{" "}
-              by clicking on your profile picture and generate a token.
-            </p>
-            <img
-              src="apiKeys.png"
-              alt=""
-              className="w-full rounded-lg h-96 border-2 border-border-color mt-4"
-            />
-          </SharkioDocsSection>
-
-          <SharkioDocsSection title="Install Sharkio-cli" sectionNumber={3}>
-            Install the Sharkio-cli.
-            <div className="flex flex-col w-full mt-2">
-              <CopyBlock
-                language="bash"
-                text={`npm i -g sharkio-cli`}
-                showLineNumbers={false}
-                theme={a11yDark}
-                wrapLines={true}
-                codeBlock
-              />
-            </div>
-          </SharkioDocsSection>
-
-          <SharkioDocsSection title="Sharkio Login" sectionNumber={4}>
-            Login to Sharkio-cli, the session will be saved in your home folder.
-            <div className="flex flex-col w-full my-2">
-              <CopyBlock
-                language="bash"
-                text={`sharkio login`}
-                showLineNumbers={false}
-                theme={a11yDark}
-                wrapLines={true}
-                codeBlock
-              />
-            </div>
-            {"You'll"} be prompted to enter your email and the precious token{" "}
-            {"you've "}secured.
-          </SharkioDocsSection>
-          {/* 
-          <div className="flex text-2xl font-bold font-mono mb-4 text-center bg-secondary p-2 px-4 rounded-lg">
-            Usage
-          </div>
-
-          <SharkioDocsSection title={"1. Start Your API 🚦"}>
-            <p>
-              Set sail and start the APIs you want to use Sharkio with as a
-              proxy. Sharkio will act as your sonar, recording all the traffic
-              and redirecting it back to the APIs.
-            </p>
-          </SharkioDocsSection>
-
-          <SharkioDocsSection title="2. Create Sniffer to Your API 🕵️‍♂️">
-            <p>
-              Time to deploy your sniffer - your very own submarine for
-              exploring the ocean of HTTP traffic. Execute the following
-              command, giving your sniffer a name and assigning it a port:
-            </p>
-            <pre>
-              <code>
-                sharkio create sniffer -p {"<port>"} -n {"<name>"}
-              </code>
-            </pre>
-          </SharkioDocsSection>
-
-          <SharkioDocsSection title="3. Start Your Sniffer 🚀">
-            <p>
-              With this command, {"you'll"} launch your sniffer into the depths:
-            </p>
-            <pre>
-              <code>sharkio start sniffer</code>
-            </pre>
-            <p>
-              This creates an underwater tunnel using ngrok, channeling the
-              traffic from the proxy to the tunnel and then to the local API.
-              Watch as the data flows like a current through your sniffer.
-            </p>
-          </SharkioDocsSection>
-
-          <SharkioDocsSection title="4. Redirect Other Services to Subdomain 🔀">
-            <p>
-              In the final leg of your journey, redirect other services to the
-              {"proxy's"} subdomain by adjusting the .env variable. This ensures
-              all traffic flows through Sharkio, allowing you to monitor every
-              ripple and wave.
-            </p>
-          </SharkioDocsSection> */}
+        Port: The port your API is running on.{"\n"}
+        Name: The name of your sniffer.
+        <div className="text-lg font-serif mt-2">
+          {"You'll"} be given a unique subdomain for your sniffer. This is where
+          you will redirect your traffic to.
         </div>
-      </div>
-    </div>
+      </SharkioDocsSection>
+
+      <SharkioDocsSection title="Start Your Sniffer" sectionNumber={3}>
+        Start your sniffer by running the following command:
+        <div className="flex flex-col w-full my-2">
+          <CopyBlock
+            language="bash"
+            text={`sharkio start sniffer`}
+            showLineNumbers={false}
+            theme={a11yDark}
+            wrapLines={true}
+            codeBlock
+          />
+        </div>
+      </SharkioDocsSection>
+
+      <SharkioDocsSection
+        title="Redirect Other Services to Subdomain"
+        sectionNumber={4}
+      >
+        Redirect your all the services that communicate with your API to the new
+        subdomain.{"\n"}
+        Now you can open the Sharkio dashboard and start building faster.
+      </SharkioDocsSection>
+    </SharkioDocsPageTemplate>
   );
 };
 
@@ -295,29 +293,49 @@ const SharkioDocsSection = ({ title, children, sectionNumber }) => {
   return (
     <section className="flex flex-col mb-8">
       <div className="flex w-full flex-row mb-2">
-        <div className="flex text font-mono bg-blue-200 px-2 rounded-lg border-blue-200 border-2 bg-opacity-40 mr-4">
+        <div className="flex text font-serif bg-blue-200 px-2 rounded-lg border-blue-200 border-2 bg-opacity-40 mr-4">
           {sectionNumber ? `${sectionNumber}` : 1}
         </div>
         <div className="text-xl font-bold whitespace-pre-line">{title}</div>
       </div>
-      <div className="text-lg font-mono mb-4 text-[#717171] whitespace-pre-line">
+      <div className="text-lg font-serif mb-4 text-[#717171] whitespace-pre-line">
         {children}
       </div>
     </section>
   );
 };
 
-const DocsNavigation = () => {
+const SharkioDocsNavigation = () => {
   return (
-    <div className="flex flex-col w-1/4 p-4">
-      <div className="flex text-2xl font-bold font-mono mb-4 text-center bg-secondary p-2 px-4 rounded-lg">
-        Setup
+    <div className="flex h-full flex-col w-1/5 border-r bg-secondary border-border-color hidden sm:inline-flex ">
+      <div className="flex text-lg font-serif text-center items-center border-b border-border-color hover:cursor-pointer hover:bg-tertiary">
+        <div className="flex flex-1 active:scale-105 px-4 py-2">
+          <TfiSettings className="w-6 h-6 mr-2" />
+          Setup
+        </div>
       </div>
-      <div className="flex text-2xl font-bold font-mono mb-4 text-center bg-secondary p-2 px-4 rounded-lg">
-        Usage
+      <div className="flex text-lg font-serif text-center items-center border-b border-border-color hover:cursor-pointer hover:bg-tertiary">
+        <div className="flex flex-1 active:scale-105 px-4 py-2">
+          <AiOutlinePlayCircle className="w-6 h-6 mr-2" />
+          Getting Started
+        </div>
       </div>
     </div>
   );
 };
 
+const SharkioDocsPageTemplate = ({ children, title, subTitle }) => {
+  return (
+    <div className="flex flex-row w-full h-full">
+      <SharkioDocsNavigation />
+      <div className="p-4 flex flex-col flex-1">
+        <div className="flex text-4xl font-serif ">{title}</div>
+        <div className="flex w-full border-b border-border-color my-6"></div>
+        <div className="text-lg font-serif mb-4 text-[#717171]">{subTitle}</div>
+
+        <div className="flex flex-col flex-1 p-4">{children}</div>
+      </div>
+    </div>
+  );
+};
 export default App;
