2c0b657287bd3f0ece5c1a8f74d5e9960eb2e33a
yair <yair.haras@gmail.com>
2023-11-09 17:58:08 +0200

    Added chat component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,89 @@ import { SharkioDocsSetupPage } from "./pages/docs/SharkioDocsSetupPage";
 import SniffersPage from "./pages/sniffers/SniffersPage";
 import { useAuthStore } from "./stores/authStore";
 import { useThemeStore } from "./stores/themeStore";
+import { BsSend } from "react-icons/bs";
+import { SiOpenai } from "react-icons/si";
+
+const ChatPage = () => {
+  const [message, setMessage] = React.useState("");
+  const [messages, setMessages] = React.useState([
+    {
+      message: "Hello, how can I help you?",
+      type: "bot",
+    },
+  ]);
+
+  const onSendMessage = () => {
+    const type = "user";
+    setMessages([{ message, type }, ...messages]);
+    setMessage("");
+  };
+
+  return (
+    <div className="flex flex-col p-4 w-full items-center h-[calc(100%-56px)]">
+      <div className="flex flex-col-reverse overflow-y-auto md:w-3/4 mb-4 h-full">
+        {messages.length === 0 && <NoMessages />}
+        {messages.reverse().map(({ message, type }, index) => {
+          if (type === "bot") {
+            return <BotMessage key={index} content={message} />;
+          } else {
+            return <UserMessage key={index} content={message} />;
+          }
+        })}
+      </div>
+      <div className="flex justify-center items-center border border-border-color rounded-2xl px-4 py-2 md:w-3/4">
+        <input
+          className="w-full bg-transparent border-none outline-none"
+          placeholder="Type a question..."
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              onSendMessage();
+            }
+          }}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <div
+          className="flex flex-row items-center justify-center bg-blue-300 rounded-lg h-8 w-8 hover:bg-blue-400 cursor-pointer active:bg-blue-500"
+          onClick={onSendMessage}
+        >
+          <BsSend className="text-[#fff]" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const BotMessage = ({ content }) => {
+  return (
+    <div className="flex flex-row items-center justify-start mb-4 ">
+      <div className="flex flex-col items-start justify-start bg-blue-300 rounded-e-xl rounded-br-xl rounded-tl-xl p-2">
+        <span className="text-tertiary text-sm">{content}</span>
+      </div>
+    </div>
+  );
+};
+
+const UserMessage = ({ content }) => {
+  return (
+    <div className="flex flex-row items-center justify-end mb-4">
+      <div className="flex flex-col items-end justify-start bg-gray-300 rounded-s-xl rounded-bl-xl rounded-tr-xl p-2">
+        <span className="text-tertiary text-sm">{content}</span>
+      </div>
+    </div>
+  );
+};
+
+const NoMessages = () => {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center">
+      <SiOpenai className="text-gray-400 text-4xl mb-2" />
+      <span className="text-gray-400 text-lg">
+        Ask questions about your APIs
+      </span>
+    </div>
+  );
+};
 
 function App(): React.JSX.Element {
   const { mode } = useThemeStore();
@@ -31,6 +114,7 @@ function App(): React.JSX.Element {
       { path: routes.SNIFFER_ENDPOINT, element: <SniffersPage /> },
       { path: routes.SNIFFER_ENDPOINT_INVOCATION, element: <SniffersPage /> },
       { path: routes.LIVE_INVOCATION, element: <SniffersPage /> },
+      { path: routes.CHAT, element: <ChatPage /> },
     ];
 
     return routesWithAuth.map(({ path, element }) => (
--- client/src/components/page-template/page-template.tsx
+++ client/src/components/page-template/page-template.tsx
@@ -2,7 +2,8 @@ import React, { PropsWithChildren } from "react";
 import { useAuthStore } from "../../stores/authStore";
 import { Navbar } from "../navbar/navbar";
 import { SideBar } from "../sidebar/sidebar";
-import { BiTerminal } from "react-icons/bi";
+import { SiOpenai } from "react-icons/si";
+import { useNavigate } from "react-router-dom";
 
 export const PageTemplate: React.FC<
   PropsWithChildren & { isSideBar?: boolean }
@@ -13,7 +14,7 @@ export const PageTemplate: React.FC<
     <div className="flex flex-col min-h-screen">
       <div className="flex flex-row h-full w-full flex-1">
         {user && isSideBar && <SideBar />}
-        <div className="flex flex-col flex-1 bg-tertiary">
+        <div className="flex flex-col w-full bg-tertiary h-[calc(100vh-40px)]">
           <Navbar />
           {children}
         </div>
@@ -24,12 +25,20 @@ export const PageTemplate: React.FC<
 };
 
 const BottomBar: React.FC = () => {
+  const navigate = useNavigate();
+
+  const onChatClick = () => {
+    navigate("/chat");
+  };
   return (
-    <div className="sticky bottom-0 flex-row w-full bg-secondary h-10 border-t-[0.1px] border-border-color">
+    <div className="sticky bottom-0 flex-row w-full bg-secondary h-10 border-t border-border-color">
       <div className="flex flex-row w-full h-full items-center justify-between px-4">
         <div className="text-[#fff]">© 2023 Sharkio</div>
         <div className="flex flex-row items-center">
-          <BiTerminal className="text-[#fff] text-2xl hover:bg-border-color rounded-md hover:cursor-pointer active:scale-110" />
+          <SiOpenai
+            className="text-[#fff] text-2xl hover:bg-border-color rounded-md hover:cursor-pointer active:scale-110"
+            onClick={onChatClick}
+          />
         </div>
       </div>
     </div>
