813df9e1e9794f7e0c3c6fb187f10f1a350959b1
ido peled <Idop9081@gmail.com>
2023-11-18 19:16:51 +0200

    url copy when click on url

--- client/src/pages/sniffers/InvocationUpperBar.tsx
+++ client/src/pages/sniffers/InvocationUpperBar.tsx
@@ -8,6 +8,7 @@ import { Sniffer } from "../../stores/sniffersStores";
 import { executeInvocation } from "../../api/api";
 import { useState } from "react";
 import { LoadingIcon } from "./LoadingIcon";
+import { AiOutlineCopy } from "react-icons/ai";
 
 type InvocationUpperBarProps = {
   activeInvocation?: InvocationType;
@@ -43,16 +44,29 @@ export const InvocationUpperBar = ({
   };
   return (
     <>
-      {activeSniffer && (
-        <Tooltip title="Sniffer's domain" placement="top" arrow>
-          <div className="flex flex-row items-center space-x-4 mb-4">
+     {activeSniffer && (
+        <Tooltip
+          onClick={() =>
+            navigator.clipboard.writeText(domainPath(activeSniffer.subdomain))
+          }
+          title="Copy Sniffer's domain"
+          placement="top"
+          arrow
+        >
+          <div className="flex flex-row items-center space-x-4 mb-4 cursor-pointer">
             <MdDomain className="text-blue-500 text-2xl p-1" />
             <div className="text-sm text-gray-500">
+              <span className="text-white ">Sniffer's Domain: </span>
               {domainPath(activeSniffer.subdomain)}
             </div>
+            <AiOutlineCopy title="Copy domain" />
           </div>
         </Tooltip>
+
+        //you want click to copy only on the icon or the text
       )}
+
+
       <div className="flex flex-row items-center space-x-4">
         {selectIconByMethod(activeInvocation?.method || "GET")}
         <TextField
