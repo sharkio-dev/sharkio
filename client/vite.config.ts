ebf97340efc0e5b84291ebdc53a48a8650a8d8ee
Ido David <21335259+idodav@users.noreply.github.com>
2023-11-19 17:06:45 +0200

    fix vite hmr circular deps (#258)
    * format
    
    * feat: validate env vars
    
    * format
    
    * fix: onproxyres self handle response wait for db
    
    * format
    
    * fix: vite hmr circular deps
    
    * format
    
    ---------
    
    Co-authored-by: IdoDavidAK <ido@arborknot.io>

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import circleDependency from "vite-plugin-circular-dependency";
 
 export default () => {
   return defineConfig({
-    plugins: [react(), basicSsl()],
+    plugins: [
+      circleDependency({
+        circleImportThrowErr: false,
+      }),
+      react(),
+      basicSsl(),
+    ],
     server: {
       port: +(process.env.VITE_SERVER_PORT ?? 8123),
     },
