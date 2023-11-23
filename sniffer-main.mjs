8c896a611917ba8bf720c0b5d76ff4f461842149
IdoDavidAK <ido@arborknot.io>
2023-11-23 12:29:02 +0200

    chore: delete irrelevant files

--- sniffer-main.mjs
+++
@@ -1,8 +0,0 @@
-import { SnifferManagerController } from "server";
-import dotenv from "dotenv";
-dotenv.config();
-
-const adminPort = +(process.env.ADMIN_PORT ?? 5012);
-const snifferController = new SnifferManagerController();
-
-snifferController.start(adminPort);
