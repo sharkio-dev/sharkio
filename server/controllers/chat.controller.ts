47507353bc6790106f466ffacd725022d7f219cb
yair <yair.haras@gmail.com>
2023-11-25 18:21:19 +0200

    Fix 3

--- server/controllers/chat.controller.ts
+++ server/controllers/chat.controller.ts
@@ -32,7 +32,7 @@ export class ChatController {
   constructor(
     private readonly snifferManager: SnifferService,
     private readonly requestService: EndpointService,
-    private readonly chatService: ChatService,
+    private readonly chatService: ChatService
   ) {
     this.openai = new OpenAI({
       apiKey: process.env.OPEN_API_KEY ?? "",
@@ -61,7 +61,7 @@ export class ChatController {
         await this.chatService.addSystemMessage(
           userId,
           chat.id,
-          completionMessage,
+          completionMessage
         );
         res.status(200).json({ chatId: chat.id, content: completionMessage });
       } else {
@@ -76,7 +76,9 @@ export class ChatController {
         const { chatId } = req.params;
         const { content } = req.body;
 
-        const messages = await this.chatService.loadMessagesByChatId(chatId);
+        const messages = await this.chatService.loadMessagesByChatId(
+          chatId as string
+        );
         await this.chatService.addUserMessage(userId, chatId, content);
 
         const userMessage: ChatCompletionMessageParam = {
@@ -95,7 +97,7 @@ export class ChatController {
           await this.chatService.addSystemMessage(
             userId,
             chatId,
-            completionMessage,
+            completionMessage
           );
           res.status(200).json({ chatId, content: completionMessage });
         } else {
