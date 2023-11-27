11853ecdc2f7bc6bb9c922b6acc5d398bdf9730c
‪yair haras‬‏ <54411604+ypotter@users.noreply.github.com>
2023-11-27 22:55:21 +0200

    Fix https agent (#297)
    * Fix
    
    * Added fix

--- server/services/request/request.service.ts
+++ server/services/request/request.service.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useLog } from "../../lib/log";
 import { RequestRepository } from "../../model/request/request.model";
+import https from "https";
 
 const log = useLog({
   dirname: __dirname,
@@ -14,16 +15,28 @@ type ExecutionType = {
   subdomain: string;
 };
 
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
 export class RequestService {
   constructor(private readonly requestRepository: RequestRepository) {}
 
   async execute({ method, url, headers, body, subdomain }: ExecutionType) {
+    console.log("execute", {
+      method,
+      url,
+      headers,
+      body,
+      subdomain,
+    });
     await axios
       .request({
         method,
         url: `https://${subdomain}.${process.env.PROXY_SERVER_DOMAIN}` + url,
         headers,
-        data: body,
+        data: "",
+        httpsAgent: agent,
       })
       .catch((e) => {
         log.error(e);
