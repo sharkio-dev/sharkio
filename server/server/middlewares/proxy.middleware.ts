b4df9f84b435ee9b7c52cf0a34aaa51eee993b97
ido peled <Idop9081@gmail.com>
2023-12-04 15:53:59 +0200

    changed button style

--- server/server/middlewares/proxy.middleware.ts
+++ server/server/middlewares/proxy.middleware.ts
@@ -15,7 +15,7 @@ export class ProxyMiddleware {
 
   constructor(
     private readonly snifferService: SnifferService,
-    private readonly requestInterceptor: RequestInterceptor
+    private readonly requestInterceptor: RequestInterceptor,
   ) {
     this.proxyMiddleware = createProxyMiddleware({
       router: this.chooseRoute.bind(this),
@@ -53,7 +53,7 @@ export class ProxyMiddleware {
                       headers: proxyRes.headers,
                       statusCode: proxyRes.statusCode,
                     },
-                    testExecutionId
+                    testExecutionId,
                   )
                   .then((data) => {
                     res.end(body?.toString() || "");
@@ -65,12 +65,12 @@ export class ProxyMiddleware {
               } else {
                 res.end(body);
               }
-            }.bind(this)
+            }.bind(this),
           );
         } catch (e) {
           logger.error(
             "failed to capture response for invocation id" + invocationId,
-            e
+            e,
           );
         }
       },
@@ -81,7 +81,7 @@ export class ProxyMiddleware {
     const host = req.hostname;
     const subdomain = host.split(".")[0];
     const selectedSniffer = await this.snifferService.findBySubdomain(
-      subdomain
+      subdomain,
     );
     req.headers["x-sharkio-port"] = selectedSniffer?.port.toString();
 
