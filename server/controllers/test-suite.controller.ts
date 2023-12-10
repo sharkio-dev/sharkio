ceae161a6d92dbfa79593c0c930cc7ceb0b0a0df
ido <idonkl100@gmail.com>
2023-12-10 16:28:13 +0200

    format

--- server/controllers/test-suite.controller.ts
+++ server/controllers/test-suite.controller.ts
@@ -166,9 +166,8 @@ export class TestSuiteController {
       catchAsync(async (req: Request, res: Response) => {
         try {
           const { testSuiteId, testId } = req.params;
-          const testSuite = await this.testService.getByTestSuiteId(
-            testSuiteId,
-          );
+          const testSuite =
+            await this.testService.getByTestSuiteId(testSuiteId);
           if (!testSuite) {
             return res.status(404).send();
           }
@@ -354,9 +353,8 @@ export class TestSuiteController {
           const { testSuiteId, testId } = req.params;
           const userId = res.locals.auth.user.id;
 
-          const testSuite = await this.testService.getByTestSuiteId(
-            testSuiteId,
-          );
+          const testSuite =
+            await this.testService.getByTestSuiteId(testSuiteId);
           if (!testSuite) {
             return res.status(404).send();
           }
