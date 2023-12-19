f8c1b9a7237cc3f139d6a6c1e69e2888c6635641
ido <idonkl100@gmail.com>
2023-12-19 09:47:41 +0200

    format

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -13,5 +13,5 @@ if (import.meta.env.VITE_FULLSTORY_ORG_ID) {
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
