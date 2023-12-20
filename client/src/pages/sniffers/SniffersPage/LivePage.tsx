a6c06430356cc5f37e3056296f9190e403aca353
ido <idonkl100@gmail.com>
2023-12-20 14:36:26 +0200

    feat: resizable panels

--- client/src/pages/sniffers/SniffersPage/LivePage.tsx
+++ client/src/pages/sniffers/SniffersPage/LivePage.tsx
@@ -5,6 +5,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useSniffersStore } from "../../../stores/sniffersStores";
 import { useSnackbar } from "../../../hooks/useSnackbar";
 import { routes } from "../../../constants/routes";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 export const LivePage = () => {
   const { invocationId } = useParams();
@@ -43,30 +44,45 @@ export const LivePage = () => {
     (id: string) => {
       navigator(`${routes.LIVE_INVOCATIONS}/${id}`);
     },
-    [invocationId],
+    [invocationId]
   );
-  const bottomBarHeight = !invocationId
-    ? "h-1/1 max-h-[calc(100vh-56px)]"
-    : "h-1/3 max-h-[calc(33vh-16px)]";
 
   return (
     <div className="flex flex-row w-full h-[calc(100vh-96px)] max-h-[calc(vh-96px)]">
       {(invocations.length > 0 || loadingInvocations) && (
-        <div className={`flex flex-col w-full`}>
+        <div className={`flex flex-col w-full h-full`}>
           {snackBar}
-          {invocationId && (
-            <div className="flex flex-col p-4 px-4 border-b border-border-color h-2/3 max-h-[calc(67vh-56px)]">
-              <InvocationUpperBar activeInvocation={invocation} />
+          <PanelGroup direction={"vertical"}>
+            <Panel
+              defaultSize={invocationId ? 40 : 0}
+              minSize={25}
+              className={invocationId ? `max-h-full` : "max-h-0"}
+            >
+              {invocationId && (
+                <div className="flex flex-col p-4 px-4 border-b border-border-color h-full">
+                  <InvocationUpperBar activeInvocation={invocation} />
+                </div>
+              )}
+            </Panel>
+
+            <div className="relative h-[1px]  w-full  hover:bg-blue-300">
+              <PanelResizeHandle
+                className={`h-[30px] w-full absolute top-[-15px] left-0 `}
+              />
             </div>
-          )}
-          <div className={`flex flex-col p-2 px-4 ${bottomBarHeight}`}>
-            <InvocationsBottomBar
-              title={"Live Invocations"}
-              activeInvocation={invocation}
-              setActiveInvocation={onInvocationClick}
-              refresh={() => loadInvocations()}
-            />
-          </div>
+            <Panel minSize={10}>
+              <div
+                className={`flex flex-col p-2 px-4 max-h-full overflow-y-auto`}
+              >
+                <InvocationsBottomBar
+                  title={"Live Invocations"}
+                  activeInvocation={invocation}
+                  setActiveInvocation={onInvocationClick}
+                  refresh={() => loadInvocations()}
+                />
+              </div>
+            </Panel>
+          </PanelGroup>
         </div>
       )}
       {invocations.length === 0 && !loadingInvocations && (
