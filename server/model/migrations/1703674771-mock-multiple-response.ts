a3fb93afff505ea3242f16bf21637060fa2ceb09
ido <idonkl100@gmail.com>
2023-12-27 13:33:51 +0200

    feat: add name field

--- server/model/migrations/1703674771-mock-multiple-response.ts
+++ server/model/migrations/1703674771-mock-multiple-response.ts
@@ -13,13 +13,14 @@ export class Migrations1703332511084 implements MigrationInterface {
           status int4 NOT NULL,
           body varchar NULL,
           headers json NULL,
+          "name" varchar NULL,
           CONSTRAINT mock_response_pk PRIMARY KEY (id),
           CONSTRAINT mock_response_fk FOREIGN KEY (mock_id) REFERENCES public.mock(id) ON DELETE CASCADE,
           CONSTRAINT mock_response_fk_1 FOREIGN KEY (sniffer_id) REFERENCES public.sniffer(id) ON DELETE CASCADE
         );
         ALTER TABLE public.mock ADD current_mock_response_id uuid NULL;
         ALTER TABLE public.mock ADD CONSTRAINT mock_fk FOREIGN KEY (current_mock_response_id) REFERENCES public.mock_response(id);
-      `,
+      `
     );
   }
 
