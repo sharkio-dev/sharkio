92bd31f4ac063156273b7d590e6874ea97a3bdb4
ido <idonkl100@gmail.com>
2023-12-27 19:55:20 +0200

    format

--- client/src/pages/mocks/MockResponseDetails.tsx
+++ client/src/pages/mocks/MockResponseDetails.tsx
@@ -14,7 +14,7 @@ export const MockResponseDetails: React.FC<MockResponseDetailsProps> = ({
   handleResponseChange,
 }) => {
   const [section, setSection] = React.useState<"Status" | "Body" | "Headers">(
-    "Body"
+    "Body",
   );
 
   return (
