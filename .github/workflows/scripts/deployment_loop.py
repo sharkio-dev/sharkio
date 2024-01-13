30f457dab27a4715e39a3c6d60ff7d586ff15238
arielrahamim <ariel_rahamim@yahoo.com>
2024-01-13 15:25:30 +0200

    staging cicd testing changes

--- .github/workflows/scripts/deployment_loop.py
+++ .github/workflows/scripts/deployment_loop.py
@@ -10,21 +10,15 @@ def build_and_push(repository, dockerfile):
     subprocess.run(docker_build, shell=True, text=True, capture_output=True)
 
 registry = os.getenv('REGISTRY')
-print("registry:" + registry)
 full_sha = os.getenv('full_sha')
-print("full_sha:" + full_sha)
 short_sha = os.getenv('short_sha')
-print("short_sha:" + short_sha)
 environment = os.getenv('ENVIRONMENT')
-print("environment:" + environment)
 github_event_before = os.getenv('github_event_before')
-print("github_sha_before:" + github_event_before)
 
 git_command = f"git diff --name-only {str(github_event_before)} {str(full_sha)} | uniq"
-print("git_command:" + git_command)
 changed_files = subprocess.run(git_command, shell=True, text=True, capture_output=True)
 changed_files_output = changed_files.stdout.strip()
-print("changed_files_output:" + changed_files_output)
+
 images = []
 
 if "client/" in changed_files_output:
@@ -48,6 +42,6 @@ if "server/" in changed_files_output:
     os.chdir('..')
     images.append("migrations")
 
-warpped_images = "'" + "{" + "images_json" + "'" + str(images) + "'" + "}" + "'"
+warpped_images = "'" + "{" + "images_json" + ":"  + "'" + str(images) + "'" + "}" + "'"
 os.environ['GITHUB_OUTPUT']=warpped_images
-
+print(warpped_images)
