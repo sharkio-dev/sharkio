576aaac5b86261352104f3e508ad4f63e2109351
arielrahamim <ariel_rahamim@yahoo.com>
2024-01-14 12:19:56 +0200

    Staging cicd (#371)
    * initial staging cicd
    
    * progressing staging cicd
    
    * Update staging-cicd.yaml
    
    * progress staging cicd
    
    * staging cicd test
    
    * fixed typo
    
    * staging cicd test2
    
    * staging cicd - main.js test2
    
    * staging cicd - main.js test3
    
    * staging cicd - main.js test4
    
    * staging cicd - main.js test4 & fix docker buildx typo
    
    * staging cicd - main.js test5 & fix docker build
    
    * staging cicd - main.js test5 & fix docker build /
    
    * staging cicd - main.js test5 & fix docker build /
    
    * staging cicd - main.js test5 & fix docker build /client/Dockerfile
    
    * staging cicd - 2
    
    * staging cicd - 3
    
    * staging cicd testing
    
    * staging cicd testing
    
    * staging cicd testing2
    
    * staging cicd testing2
    
    * staging cicd testing2
    
    * staging cicd testing2
    
    * staging cicd testing3
    
    * staging cicd testing4
    
    * staging cicd testing4
    
    * staging cicd testing4
    
    * staging cicd testing4
    
    * staging cicd testing4
    
    * staging cicd testing4
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * staging cicd testing changes
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * testing loop with index for delay
    
    * finilazation of commit
    
    * finilazation of commit
    
    * finilazation of commit
    
    * finilized changes
    
    * finished caching
    
    * finished caching
    
    * updateD
    
    * updateD
    
    ---------
    
    Co-authored-by: omerhanegbi <141716514+omerhanegbi@users.noreply.github.com>
    Co-authored-by: omer <omerh@projectcircle.co.il>

---
+++ .github/workflows/scripts/GitOps_build.py
@@ -0,0 +1,61 @@
+import os
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--dry-run', action='store_true', help='Enable dry run mode')
+args = parser.parse_args()
+
+
+registry = os.getenv('REGISTRY')
+full_sha = os.getenv('full_sha')
+short_sha = os.getenv('short_sha')
+environment = os.getenv('ENVIRONMENT')
+github_event_before = os.getenv('github_event_before')
+
+def build_and_push(repository, dockerfile):
+    docker_build=f'docker buildx build \
+    -t {registry}/{repository}:{environment}-{full_sha} \
+    -t {registry}/{repository}:{environment}-{short_sha} \
+    --push \
+    --cache-from=type=registry,ref={registry}/{repository}:latest \
+    --cache-to=type=registry,ref={registry}/{repository}:cache,mode=max \
+    -f {dockerfile} . '
+    subprocess.run(docker_build, shell=True, text=True)
+
+git_command = f"git diff --name-only {str(github_event_before)} {str(full_sha)} | uniq"
+changed_files = subprocess.run(git_command, shell=True, text=True, capture_output=True)
+changed_files_output = changed_files.stdout.strip()
+
+images = []
+
+if "client/" in changed_files_output:
+    # Change to the server directory
+    if not args.dry_run:
+        os.chdir('client/')
+        if environment == "staging":
+            build_and_push("frontend", "Dockerfile.staging")
+        else:
+            build_and_push("frontend", "Dockerfile")   
+        os.chdir('..')
+    images.append({"name": "frontend", "index": 0})
+
+if "server/" in changed_files_output:
+    # Change to the server directory
+    if not args.dry_run:
+        os.chdir('server/')
+        build_and_push("backend", "Dockerfile.backend")
+        os.chdir('..')
+    images.append({"name": "backend", "index": 1})
+
+if "server/model/" in changed_files_output:
+    # Change to the server directory
+    if not args.dry_run:
+        os.chdir('server/')
+        build_and_push("migrations", "Dockerfile.migrations")
+        os.chdir('..')
+    images.append({"name": "migrations", "index": 2})
+
+if  args.dry_run:
+    warpped_images = '{"images_builder":' + str(images).replace("'", '"') + '}'
+    print(warpped_images)
