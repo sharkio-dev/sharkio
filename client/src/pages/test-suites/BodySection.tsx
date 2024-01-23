15e5c3f6e60f890f11a79ce16f8dff026df37728
omerhanegbi <141716514+omerhanegbi@users.noreply.github.com>
2024-01-23 15:05:58 +0200

    Docs implementation (#403)
    * Finished (#373)
    
    * Production toggle (#378)
    
    * Finished
    
    * Fixed dockerfile
    
    * Finished (#373) (#377) (#379)
    
    * Update Readme.md (#385)
    
    * Finished (#386)
    
    * Finished
    
    * Fixed
    
    * feat: workspaces (#389)
    
    * untracked files on workspaces: ecdf471 feat: faker hadnlebars + repeat + compare helpers (#364)
    
    * index on workspaces: ecdf471 feat: faker hadnlebars + repeat + compare helpers (#364)
    
    * workspaces
    
    * format
    
    * feat: edit modal + workspace users CRUD
    
    * format
    
    * format
    
    * format
    
    * feat: ownerId + workspace selector change
    
    * format
    
    * fix: middleware ownerId
    
    * format
    
    * fix: orm relations + build
    
    * fix: localhost https debug
    
    * format
    
    * feat: workspace migrations
    
    * format
    
    * package lock
    
    * fix: minor changes
    
    * chore: remove unused var
    
    * fix: workspace
    
    * commit (#397)
    
    * Added copy, beautify, and format (#401)
    
    * Added copy, beautify, and format
    
    * Added
    
    * Docs (#400)
    
    * Added docosaurus
    
    * Adopted tutorial to sharkio
    
    * Added basic structure
    
    * added more docs
    
    * format
    
    * Added more docs
    
    * added more docssss
    
    * Added dockerfile
    
    * Added workflow
    
    * Added docs
    
    * updated gitops cd for new microservice docs
    
    ---------
    
    Co-authored-by: arielrahamim <ariel_rahamim@yahoo.com>
    
    ---------
    
    Co-authored-by: ‪yair haras‬‏ <54411604+ypotter@users.noreply.github.com>
    Co-authored-by: Abinand P <abinand0911@gmail.com>
    Co-authored-by: Ido David <21335259+idodav@users.noreply.github.com>
    Co-authored-by: arielrahamim <ariel_rahamim@yahoo.com>

--- client/src/pages/test-suites/BodySection.tsx
+++ client/src/pages/test-suites/BodySection.tsx
@@ -1,56 +1,92 @@
 import Editor from "@monaco-editor/react";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
 import { LoadingIcon } from "../sniffers/LoadingIcon";
+import { SelectComponent } from "../../components/select-component/SelectComponent";
+import { MdOutlineCopyAll } from "react-icons/md";
+import { Button } from "@mui/material";
+import { useSnackbar } from "../../hooks/useSnackbar";
 
 type BodySectionProps = {
   body: any;
   language?: string;
   onBodyChange?: (body: any) => void;
+  showButtons?: boolean;
 };
 
 export const BodySection = ({
   body,
   language,
   onBodyChange,
+  showButtons = true,
 }: BodySectionProps) => {
-  const params = useParams();
-  const [invocationId, setInvoationId] = useState(params.invocationId);
+  const [type, setType] = useState("json");
   const [editor, setEditor] = useState<any>(null);
-  useEffect(() => {
-    if (!editor) return;
-    editor.trigger("anyString", "editor.action.formatDocument", {});
-    setInvoationId(invocationId);
-  }, [editor]);
+  const { show: showSnackbar, component: snackBar } = useSnackbar();
 
   const onChangeBodyValue = (value: any) => {
     try {
       onBodyChange?.(value);
     } catch (error) {}
   };
-  const type =
-    language ?? (typeof body === "string" && body.includes("html"))
-      ? "html"
-      : "json";
+
+  const beautify = () => {
+    if (!editor) return;
+    editor.trigger("anyString", "editor.action.formatDocument", "");
+  };
+
+  const copyToClipboard = () => {
+    if (!editor) return;
+    navigator.clipboard.writeText(editor.getValue());
+    showSnackbar("Copied to clipboard", "success");
+  };
 
   return (
-    <Editor
-      width="100%"
-      theme="vs-dark"
-      className="h-[50vh]"
-      value={body}
-      loading={<LoadingIcon />}
-      language={type}
-      onMount={(editor) => {
-        setEditor(editor);
-      }}
-      onChange={(value) => onChangeBodyValue(value)}
-      options={{
-        readOnly: !onBodyChange,
-        minimap: {
-          enabled: false,
-        },
-      }}
-    />
+    <div className="flex flex-col h-full w-full">
+      {snackBar}
+      {showButtons && (
+        <div className="flex justify-between items-center">
+          <div className="flex w-24">
+            <SelectComponent
+              options={[
+                { label: "JSON", value: "json" },
+                { label: "XML", value: "xml" },
+                { label: "TEXT", value: "text" },
+                { label: "HTML", value: "html" },
+              ]}
+              value={type}
+              setValue={setType}
+              variant="standard"
+            />
+          </div>
+          <div className="flex items-center space-x-2">
+            <Button variant="text" onClick={beautify}>
+              Beautify
+            </Button>
+            <MdOutlineCopyAll
+              className="text-2xl cursor-pointer"
+              onClick={copyToClipboard}
+            />
+          </div>
+        </div>
+      )}
+      <Editor
+        width="100%"
+        theme="vs-dark"
+        className="h-[50vh]"
+        value={body}
+        loading={<LoadingIcon />}
+        language={language || type}
+        onMount={(editor) => {
+          setEditor(editor);
+        }}
+        onChange={(value) => onChangeBodyValue(value)}
+        options={{
+          formatOnPaste: true,
+          minimap: {
+            enabled: false,
+          },
+        }}
+      />
+    </div>
   );
 };
