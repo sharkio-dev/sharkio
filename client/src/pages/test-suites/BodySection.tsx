27d81ca8011221d0272015d533b14a8953280c30
yair <yair.haras@gmail.com>
2024-01-26 17:56:42 +0200

    build fix

--- client/src/pages/test-suites/BodySection.tsx
+++ client/src/pages/test-suites/BodySection.tsx
@@ -1,5 +1,5 @@
 import Editor from "@monaco-editor/react";
-import React, { useState } from "react";
+import { useState } from "react";
 import { LoadingIcon } from "../sniffers/LoadingIcon";
 import { SelectComponent } from "../../components/select-component/SelectComponent";
 import { MdOutlineCopyAll } from "react-icons/md";
