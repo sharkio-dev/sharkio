371d516e85ec980a8eb8005202fa928d32e5069a
ido <idonkl100@gmail.com>
2024-01-31 16:42:43 +0200

    feat: more comparators

--- server/services/test-flow/test-flow-executor/node-response-validator.ts
+++ server/services/test-flow/test-flow-executor/node-response-validator.ts
@@ -4,7 +4,7 @@ import {
 } from "../../../dto/in/test-flow.dto";
 import { TestFlowNode } from "../../../model/entities/test-flow/TestFlowNode";
 import { AxiosResponse } from "axios";
-import { get } from "lodash";
+import { get, includes } from "lodash";
 
 export type AssertionResult = {
   passed: TestFlowAssertionResult[];
@@ -21,6 +21,18 @@ export class NodeResponseValidator {
   constructor() {
     this.assertionHandlers = {
       eq: this.eq,
+      ne: this.ne,
+      seq: this.eq,
+      sne: this.ne,
+      gt: this.gt,
+      gte: this.gte,
+      lt: this.lt,
+      lte: this.lte,
+      contains: this.contains,
+      not_contains: this.notContains,
+      includes: this.includes,
+      not_includes: this.notIncludes,
+      typeof: this.typeof,
     };
   }
 
@@ -71,6 +83,69 @@ export class NodeResponseValidator {
   }
 
   eq(expected: any, data: any) {
+    return expected == data;
+  }
+
+  ne(expected: any, data: any) {
+    return expected != data;
+  }
+
+  seq(expected: any, data: any) {
     return expected === data;
+  }
+
+  sne(expected: any, data: any) {
+    return expected !== data;
+  }
+
+  gt(expected: any, data: any) {
+    return data > expected;
+  }
+
+  contains(expected: any, data: any) {
+    if (typeof data === "string") {
+      return data.includes(expected);
+    }
+
+    return false;
+  }
+
+  notContains(expected: any, data: any) {
+    if (typeof data === "string") {
+      return !data.includes(expected);
+    }
+
+    return false;
+  }
+  includes(expected: any, data: any) {
+    if (Array.isArray(data)) {
+      return !data.includes(expected);
+    }
+
+    return false;
+  }
+
+  notIncludes(expected: any, data: any) {
+    if (Array.isArray(data)) {
+      return !data.includes(expected);
+    }
+
+    return false;
+  }
+
+  gte(expected: any, data: any) {
+    return data >= expected;
+  }
+
+  lt(expected: any, data: any) {
+    return data < expected;
+  }
+
+  lte(expected: any, data: any) {
+    return data <= expected;
+  }
+
+  typeof(expected: any, data: any) {
+    return typeof data === expected;
   }
 }
