d88741bf1ae4b5c3513c93a797c3996c8e9afd4f
ido <idonkl100@gmail.com>
2024-02-08 16:09:38 +0200

    fix: text button flex positioning

--- client/src/components/TextButton.tsx
+++ client/src/components/TextButton.tsx
@@ -8,7 +8,7 @@ interface TextButtonProps {
 export const TextButton: React.FC<TextButtonProps> = ({ text, onClick }) => {
   return (
     <div
-      className="flex self-start flex-row items-center space-x-2 px-2 cursor-pointer min-w-32"
+      className="flex flex-row items-center space-x-2 px-2 cursor-pointer min-w-32"
       onClick={onClick}
     >
       <AiOutlinePlus className="flex text-green-400 hover:bg-border-color rounded-md hover:cursor-pointer" />
