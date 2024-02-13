eb74feb2c1adbf14eb3029c25914563b27907202
Tamir Abutbul <1tamir198@gmail.com>
2024-02-13 22:57:07 +0200

    Adding missing support for some url formats

--- client/src/utils/ValidateHttpUrl.tsx
+++ client/src/utils/ValidateHttpUrl.tsx
@@ -1,5 +1,5 @@
 export const validateHttpUrlFormat = (string: string) => {
-  const domainPattern = /^https?:\/\/[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+$/;
+  const domainPattern = /^https?:\/\/[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*$/;
   const localhostPattern = /^(http|https):\/\/localhost:\d+$/;
 
   return domainPattern.test(string) || localhostPattern.test(string);
