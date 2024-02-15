e1bec743d6e5658d76e8115aa868d7263d4cbf0c
ido <idonkl100@gmail.com>
2024-02-15 13:46:09 +0200

    fix: migratoin if exists

--- server/model/migrations/1706790372503-remove-fkey.ts
+++ server/model/migrations/1706790372503-remove-fkey.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class Workspaces1706790372502 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(`
-    ALTER TABLE public.endpoint DROP CONSTRAINT "FK_e197baa1e08f5c75e7e34688040";
+    ALTER TABLE public.endpoint DROP CONSTRAINT IF EXISTS "FK_e197baa1e08f5c75e7e34688040";
 `);
   }
 
