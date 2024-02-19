63674f00f3323eeed971c0bc9b26a4c999523ce4
yonatanfurmanassa <james8999123456789@gmail.com>
2024-02-19 11:33:53 +0200

    added a copylink component

---
+++ client/src/components/copy-link-component/CopyLink.tsx
@@ -0,0 +1,32 @@
+import { Tooltip } from "@mui/material";
+import { useState } from "react";
+import { CiLink } from "react-icons/ci";
+
+
+type CopyLinkProps = {
+    link: string
+}
+
+
+export const CopyLinkComponent = ({ link }: CopyLinkProps) => {
+
+    const [copySuccess, setCopySuccess] = useState(false);
+
+
+    const handleCopyClick = (event: any) => {
+        event.stopPropagation();
+        navigator.clipboard.writeText(`${link}`).then(() => {
+            setCopySuccess(true);
+            setTimeout(() => setCopySuccess(false), 2000);
+        });
+    };
+
+
+    return <div>
+        <Tooltip title={copySuccess ? "Link Copied!" : "Copy Link"} placement="top-end">
+            <div><CiLink onClick={handleCopyClick} /></div>
+        </Tooltip>
+    </div>
+}
+
+export default CopyLinkComponent
