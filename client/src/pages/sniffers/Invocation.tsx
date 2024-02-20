bb389264b3a6d810fdf305a7e7f9424e6e1dffbc
ido <idonkl100@gmail.com>
2024-02-20 11:49:55 +0200

    format

--- client/src/pages/sniffers/Invocation.tsx
+++ client/src/pages/sniffers/Invocation.tsx
@@ -27,10 +27,9 @@ export const Invocation = ({
   isSelected,
   onClick,
   date,
-  invocationLink
+  invocationLink,
 }: InvocationProps) => {
   const [copySuccess, setCopySuccess] = useState(false);
-
 
   // Function to handle the copy action
   const handleCopyClick = (event: any) => {
@@ -42,22 +41,31 @@ export const Invocation = ({
   };
   return (
     <div
-      className={`flex flex-row px-2 hover:bg-secondary cursor-pointer active:bg-primary items-center hover:rounded-md justify-between border-b border-border-color ${isSelected ? "bg-primary" : ""}`}
+      className={`flex flex-row px-2 hover:bg-secondary cursor-pointer active:bg-primary items-center hover:rounded-md justify-between border-b border-border-color ${
+        isSelected ? "bg-primary" : ""
+      }`}
       onClick={onClick} // Main onClick applied here for the whole div
     >
-      <div className={`flex flex-row items-center space-x-1.5 ${isSelected ? "bg-primary" : ""}`}>
+      <div
+        className={`flex flex-row items-center space-x-1.5 ${
+          isSelected ? "bg-primary" : ""
+        }`}
+      >
         {selectIconByMethod(method)}
         <div className="flex text-sm max-w-full overflow-hidden overflow-ellipsis whitespace-nowrap">
           <div className="cursor-pointer flex flex-col items-center space-y-1">
-            <Tooltip title={copySuccess ? "Link Copied!" : "Copy Link"} placement="top-end">
-              <div><CiLink onClick={handleCopyClick} /></div>
+            <Tooltip
+              title={copySuccess ? "Link Copied!" : "Copy Link"}
+              placement="top-end"
+            >
+              <div>
+                <CiLink onClick={handleCopyClick} />
+              </div>
             </Tooltip>
           </div>
         </div>
         <Tooltip title={url} placement="top-start">
-          <div className="cursor-pointer" >
-            {url}
-          </div>
+          <div className="cursor-pointer">{url}</div>
         </Tooltip>
       </div>
       <div className="flex flex-row items-center space-x-4">
